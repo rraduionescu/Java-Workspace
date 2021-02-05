a5b271c87d6af9f8d85363bb0aaca104d53e7ddd
Radu <ionescu.radu.18@gmail.com>
2021-02-05 22:46:59 +0200

    n

--- Java Training/C1/2/src/ro/ionescu/radu/Main.java
+++ Java Training/C1/2/src/ro/ionescu/radu/Main.java
@@ -5,5 +5,25 @@ public class Main
 	public static void main(String[] args)
 	{
 		System.out.println("Hello");
+
+		byte b;
+		short s;
+		int i;
+		long l;
+		float f;
+		double d;
+		char c;
+		boolean x;
+
+		String st = "4";
+		System.out.println("String literal " + st);
+
+		i = Integer.parseInt(st);
+		System.out.println("Integer        " + i);
+
+		l = 730_873_101;
+		System.out.println("Long           " + l);
+
+		c = 'x';
 	}
 }
